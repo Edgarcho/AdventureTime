888c3cd24833e8ac30b88f3a9359dfc14169bb0a
Edgar Hernandez and Samuel Gespass <samuel@>
2018-01-04 10:49:45 -0800

    pass test for returning string

--- src/test/java/AdventureTimeTest.java
+++ src/test/java/AdventureTimeTest.java
@@ -28,6 +28,6 @@ public class AdventureTimeTest {
     @Test
     public void returnMove_returnsString_true() throws Exception {
         NewGame testNewGame = new NewGame("string");
-        assertEquals("", testNewGame.returnMove("Let's go on adventures together!").getClass().toString());
+        assertEquals("class java.lang.String", testNewGame.returnMove("Let's go on adventures together!").getClass().toString());
     }
 }
